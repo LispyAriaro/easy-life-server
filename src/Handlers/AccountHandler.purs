599577c4092a2864fde0fe6fad02e4945c2c1ac6
Efe Ariaroo <efeariaroo1@gmail.com>
2018-08-19 00:20:54 +0100

    Can now return db query results to http response

--- src/Handlers/AccountHandler.purs
+++ src/Handlers/AccountHandler.purs
@@ -2,23 +2,20 @@ module Handlers.AccountHandler where
 
 import Prelude
 
+import Data.Array ((!!))
 import Data.Either (Either(..))
-import Database.Postgres (Pool, Query(Query), query_, withClient, Client) as Pg
-import Effect.Class (liftEffect)
-import Effect.Aff.Class (liftAff)
 import Data.Foldable (for_)
-
+import Database.Postgres (Pool, Query(Query), query_, withClient, Client) as Pg
+import Effect.Aff (launchAff_)
+import Effect.Aff.Class (liftAff)
+import Effect.Class (liftEffect)
 import Effect.Console (log, logShow)
 import Effect.Exception (error, Error, message)
-import Effect.Aff (launchAff_)
-
+import Foreign.Generic (encodeJSON)
 import Models.User (User(..), getUserJwtPayload)
 import Node.Express.Handler (Handler, nextThrow, next)
 import Node.Express.Request (getRouteParam, getQueryParam, getBody', getOriginalUrl, setUserData, getUserData)
 import Node.Express.Response (sendJson, setStatus)
-
-import Data.Array ((!!))
-
 import Query.Users as UsersQ
 import Rest as Rest
 import Utils as Utils
@@ -38,12 +35,11 @@ signup pool conn = do
       let query = UsersQ.getAll
       liftEffect $ log $ "Db query: " <> query
 
-      liftAff $ do
-        usersWithPhone <- Pg.query_ Utils.readForeignJson (Pg.Query query :: Pg.Query User) conn
-        liftEffect $ void $ for_ usersWithPhone logShow
-
-        pure unit
+      usersWithPhone <- liftAff $ do
+        queryResults <- Pg.query_ Utils.readForeignJson (Pg.Query query :: Pg.Query User) conn
+        liftEffect $ void $ for_ queryResults logShow
+        pure queryResults
 
       sendJson {
-        status: "All done!"
+        status: encodeJSON usersWithPhone
       }
