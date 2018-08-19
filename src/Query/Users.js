538034956814b0b21e50d62f6f764cc80756d5dc
Efe Ariaroo <efeariaroo1@gmail.com>
2018-08-19 01:04:32 +0100

    can now run db query to find user by phone number

--- src/Query/Users.js
+++ src/Query/Users.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const knex = require('knex');
+const K = require('knex');
 
 var KnexFile = require('../../config/knexfile');
 // console.log("KnexFile: ", KnexFile);
@@ -11,21 +11,21 @@ knexDbConfigs['production'] = KnexFile['production'];
 
 var dbEnvVar = 'EasyLife_DbEnv';
 var dbEnv = process.env[dbEnvVar] || 'development';
-console.log("EasyLife_DbEnv: ", dbEnv);
+console.log(dbEnvVar + ": " + dbEnv + "\n");
 
-var currentDbConfig = knexDbConfigs[dbEnv];
-var configuredKnex = knex(currentDbConfig);
+var dbConfig = knexDbConfigs[dbEnv];
+var knex = K(dbConfig);
+
 
 // exports.getAll = knex('users').select('*').orderBy('first_name', 'desc');
-exports.getAll = configuredKnex('users').select('*').toSQL().sql;
+exports.getAll = knex('users').select('*').toString();
 
 exports.getBy = function (columnName) {
   return function(columnVal) {
     var queryObj = {};
     queryObj[columnName] = columnVal;
-    // console.log("queryObj: ", queryObj)
 
-    return configuredKnex('users').where(queryObj).select('*').toSQL().sql;
+    return knex('users').where(queryObj).select('*').toString();
   }
 }
 
@@ -33,10 +33,10 @@ exports.insert = function (uuid) {
   return function (username) {
     return function (phoneNumber) {
       return function (passwordHash) {
-        return configuredKnex('users').insert({
+        return knex('users').insert({
           uuid: uuid, username: username, phone_number: phoneNumber, 
           password_hash: passwordHash
-        }).toSQL().sql;
+        }).toString();
       }
     }
   }
