8059f3568f812dd7dca9f58e87b68794ef3e392a
Efe Ariaroo <efeariaroo1@gmail.com>
2018-08-25 04:36:17 +0100

    minor refactor on AccountHandler file

--- src/Handlers/AccountHandler.purs
+++ src/Handlers/AccountHandler.purs
@@ -1,31 +1,29 @@
 module Handlers.AccountHandler where
 
-import Models.User
-import Models.UserRole
-import Prelude
-import Query.InsertResult
-
 import Constants (tableNames, userTableColumns, jwtSecret, roles)
-import Data.Array (length, (!!))
 import Data.Either (Either(..))
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String.CodeUnits as Str
-import Database.Postgres (Pool, Query(Query), connect, execute, execute_, query_, queryOne_, queryValue_, withClient, release) as Pg
+import Database.Postgres (Pool, Query(Query), connect, execute_, queryOne_, query_, release) as Pg
 import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
 import Effect.Console (log, logShow)
-import Effect.Exception (error, Error, message)
+import Effect.Exception (Error)
 import FFI.BCrypt as BCrypt
 import FFI.Jwt as Jwt
 import FFI.PhoneNumber as PhoneNumber
 import FFI.UUID as UUID
 import Foreign.Generic (encodeJSON)
-import Node.Express.Handler (Handler, nextThrow, next)
-import Node.Express.Request (getRouteParam, getQueryParam, getBody', getOriginalUrl, setUserData, getUserData)
+import Models.User (User(..))
+import Models.UserRole (UserRole(..))
+import Node.Express.Handler (Handler)
+import Node.Express.Request (getBody')
 import Node.Express.Response (sendJson, setStatus)
 import Node.Process (lookupEnv)
+import Prelude (bind, discard, not, pure, show, ($), (<), (<>))
 import Query.Base as Query
+import Query.InsertResult (InsertResult(..))
 import Rest as Rest
 import Utils as Utils
 
